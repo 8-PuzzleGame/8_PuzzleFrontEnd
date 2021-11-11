c231355450d672bbe7ac34fdd886ca1ef67e0498
AhmedYoussef <Ahmed@My-HP>
2021-11-11 20:27:17 +0200

    8 puzzle code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+:root {
+  --colorD1: #181a1b;
+  --colorD2: #202e36;
+  --colorD2O5: #202e3683;
+  --colorD3: #3e5a69;
+  --colorD3O5: #3e5a6983;
+  --colorL2: #8a969c;
+  --colorL1: #a1aeb5;
+  color: var(--colorL1);
+  background-color: var(--colorD1);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.main-app-cont .nav-bar {
+  height: 5vh;
+}
+.main-app-cont .router-view {
+  height: 95vh;
+}
+.button1 {
+  display: inline-block;
+  padding: 5px;
+  border-radius: 5px;
+  width: fit-content;
+  font-weight: bolder;
+  color: white;
+  background-color: var(--colorD3);
+}
+.button1:hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+/*Scroll bar*/
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+}
+::-webkit-scrollbar-track {
+  background: var(--colorD3);
+}
+::-webkit-scrollbar-thumb {
+  background-color: var(--colorL2);
+}
+::-webkit-scrollbar-thumb:hover {
+  background-color: var(--colorL1);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,415 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="puzzle-comp-cont">
+    <div class="navImgs">
+      <img id="view" src="@/assets/view.jpg" alt="view" @click="drawImg" />
+      <img id="jumb" src="@/assets/jumb.jpg" alt="jumb" @click="drawImg" />
+      <img id="anime" src="@/assets/anime.jpg" alt="anime" @click="drawImg" />
+    </div>
+    <div class="content">
+      <div class="options">
+        <div>
+          <input class="show-nums" type="checkbox" v-model="show_numbers" />
+          <label for="checkbox">show numbers</label>
+        </div>
+        <div>
+          <label for="algorithm">Algorithm:</label>
+          <select class="algorithm" v-model="algorithm">
+            <option value="A_star">A*</option>
+            <option value="BFS">BFS</option>
+            <option value="DFS">DFS</option>
+          </select>
+        </div>
+      </div>
+      <div class="game">
+        <canvas id="backCnv" :width="cnvL" :height="cnvL"></canvas>
+        <div class="puzzle">
+          <div
+            class="section"
+            v-for="i in 9"
+            :key="i"
+            :id="'section-' + (i - 1)"
+            @click="swapSectionClick(i - 1)"
+          >
+            <canvas class="imgPart" :width="partL" :height="partL"></canvas>
+            <span v-show="show_numbers">{{ i }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="btns">
+        <div class="button1" @click="shuffleThePuzzle">Shuffle</div>
+        <div
+          class="button1 btn-sol"
+          :style="'background-color:' + solBtncolor"
+          @click="solveThePuzzle"
+        >
+          {{ solBtn }}
+        </div>
+      </div>
+      <div class="sol-info">
+        <div class="path" v-show="path" v-html="path"></div>
+        <div class="algorithm-info"></div>
+      </div>
+      <div class="message" v-html="message"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+/*
+example is
+list <= [3, 0, 1, 8, 5, 2, 6, 4, 7]
+puzzle <= [4, 1, 2, 0, 6, 3, 7, 5, 8]
+path <= [Up, Right, Right, Down, Left, Down, Right]
+run
+\u25B6
+stop
+\u25FC
+*/
+import { gsap } from "gsap";
 
+const cnvL = 600,
+  partL = cnvL / 3,
+  emp = 8;
+
+let grid = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  indexs = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  animationFlag = false,
+  runFlag = false,
+  animation = {
+    id: null,
+    interval: 600,
+    path: null,
+    pathIndex: 0,
+  },
+  sections;
+
+function swapSection(e) {
+  const eIndex = indexs[e];
+  let step = 0;
+
+  if (grid[eIndex - 3] == emp) step = -3;
+  else if (grid[eIndex + 1] == emp) step = 1;
+  else if (grid[indexs[e] + 3] == emp) step = 3;
+  else if (grid[indexs[e] - 1] == emp) step = -1;
+  else return;
+
+  indexs[e] += step;
+  swap(grid, eIndex, eIndex + step);
+  flip(sections[e], () => {
+    moveSection(e, indexs[e]);
+    moveSection(emp, eIndex);
+  });
+}
+function swap(list, i1, i2) {
+  const temp = list[i1];
+  list[i1] = list[i2];
+  list[i2] = temp;
+}
+function moveSection(index, position) {
+  sections[index].style.setProperty("grid-area", `a${position}`);
+}
+function isSolvable(list) {
+  let inversions = 0;
+  for (let i = 0; i < list.length - 1; i++) {
+    if (list[i] == 8) continue;
+    for (let j = i + 1; j < list.length; j++)
+      if (list[j] != 8 && list[i] > list[j]) inversions++;
+  }
+  return inversions % 2 == 0;
+}
+function shuffleList(list) {
+  do {
+    // for (let i = list.length - 1; i > 0; i--) {
+    //   swap(list, i, Math.floor(Math.random() * (i + 1)));
+    // }
+    list = [3, 0, 1, 8, 5, 2, 6, 4, 7];
+  } while (!isSolvable(list));
+  return list;
+}
+function orderSections(list) {
+  grid = list;
+  for (let i = 0; i < grid.length; i++) {
+    indexs[grid[i]] = i;
+    moveSection(grid[i], i);
+  }
+}
+function checkWin() {
+  return String(grid) == String([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+}
+function puzzleSolution() {
+  return ["Up", "Right", "Right", "Down", "Left", "Down", "Right"];
+}
+function flip(elements, changeFunc, vars) {
+  elements = gsap.utils.toArray(elements);
+  vars = vars || {};
+  let tl = gsap.timeline({
+      onComplete: vars.onComplete,
+      delay: vars.delay || 0,
+    }),
+    bounds = elements.map((el) => el.getBoundingClientRect()),
+    copy = {},
+    p;
+  elements.forEach((el) => {
+    el._flip && el._flip.progress(1);
+    el._flip = tl;
+  });
+  changeFunc();
+  for (p in vars) {
+    p !== "onComplete" && p !== "delay" && (copy[p] = vars[p]);
+  }
+  copy.x = (i, element) =>
+    "+=" + (bounds[i].left - element.getBoundingClientRect().left);
+  copy.y = (i, element) =>
+    "+=" + (bounds[i].top - element.getBoundingClientRect().top);
+  return tl.from(elements, copy);
+}
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: "8-puzzle",
+  data: function () {
+    return {
+      cnvL: cnvL,
+      partL: partL,
+      message: "",
+      show_numbers: false,
+      algorithm: "A_star",
+      solBtn: "Solution",
+      solBtncolor: "red",
+      path: "",
+    };
+  },
+  mounted() {
+    sections = document.getElementsByClassName("section");
+  },
+  methods: {
+    init_puzzle() {
+      grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+      orderSections(grid);
+      this.message = "";
+      this.path = "";
+      this.endAnimation();
+    },
+    drawImg(e) {
+      // scale image
+      const scaledImg = document.createElement("canvas");
+      scaledImg.width = cnvL;
+      scaledImg.height = cnvL;
+      scaledImg.getContext("2d").drawImage(e.srcElement, 0, 0, cnvL, cnvL);
+      // set background
+      document
+        .getElementById("backCnv")
+        .getContext("2d")
+        .drawImage(scaledImg, 0, 0, cnvL, cnvL);
+      // split and draw
+      const imgParts = document.getElementsByClassName("imgPart"),
+        imgPartL = cnvL / 3;
+      for (let i = 0; i < 9; i++) {
+        imgParts[i]
+          .getContext("2d")
+          .drawImage(
+            scaledImg,
+            imgPartL * (i % 3),
+            imgPartL * Math.floor(i / 3),
+            imgPartL,
+            imgPartL,
+            0,
+            0,
+            partL,
+            partL
+          );
+      }
+      this.init_puzzle();
+    },
+    swapSectionClick(e) {
+      if (animationFlag) return;
+
+      swapSection(e);
+      if (checkWin())
+        this.message = `<span>Well Done <span style="font-size: 1.2em;">&#9745;</span></span>`;
+    },
+    shuffleThePuzzle() {
+      if (runFlag) return;
+
+      this.init_puzzle();
+      grid = shuffleList(grid);
+      orderSections(grid);
+      this.message = "";
+    },
+    solveThePuzzle() {
+      if (!animationFlag) {
+        const sol = puzzleSolution();
+        this.path = sol;
+        animation.path = sol;
+        animation.pathIndex = 0;
+
+        this.solBtnStyle("Run", "green");
+        animationFlag = true;
+      } else if (runFlag) {
+        this.stopAnimation();
+        this.solBtnStyle("Run", "green");
+        runFlag = false;
+      } else {
+        this.runAnimation();
+        this.solBtnStyle("Stop", "red");
+        runFlag = true;
+      }
+    },
+    solBtnStyle(name, color) {
+      this.solBtn = name;
+      this.solBtncolor = color;
+    },
+    runAnimation() {
+      let empIndex = grid.indexOf(8);
+
+      animation.id = setInterval(() => {
+        let move = animation.path[animation.pathIndex];
+        let step = 0;
+
+        if (move === "Up") step = -3;
+        else if (move === "Down") step = 3;
+        else if (move === "Right") step = 1;
+        else if (move === "Left") step = -1;
+        else {
+          this.endAnimation();
+          return;
+        }
+
+        swapSection(grid[empIndex + step]);
+        empIndex += step;
+        animation.pathIndex++;
+
+        if (checkWin()) this.endAnimation();
+      }, animation.interval);
+    },
+    stopAnimation() {
+      clearInterval(animation.id);
+    },
+    endAnimation() {
+      this.stopAnimation();
+      this.solBtnStyle("Solution", "red");
+      animation.path = [];
+      runFlag = false;
+      animationFlag = false;
+    },
   },
 };
 </script>
+
+<style scoped>
+/* layout style */
+.puzzle-comp-cont {
+  display: flex;
+  height: 100vh;
+}
+.navImgs {
+  display: flex;
+  padding: 20px;
+  width: 25%;
+  max-width: 250px;
+  gap: 10px;
+  flex-direction: column;
+  overflow: auto;
+  background-color: var(--colorD2);
+}
+.navImgs img {
+  border-radius: 1em;
+}
+.content {
+  padding: 30px 10px 10px 10px;
+  flex: 1;
+  overflow: auto;
+}
+.game {
+  position: relative;
+  margin: auto;
+  width: 400px;
+  height: 400px;
+}
+.btns div {
+  margin: 10px 10px 10px 0;
+  user-select: none;
+}
+.sol-info {
+  margin: auto;
+  width: 90%;
+}
+.options {
+  display: inline-flex;
+  width: 400px;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.show-nums {
+  margin: 0 5px 5px 0;
+}
+.algorithm {
+  margin: 0 0px 5px 5px;
+}
+@media (max-width: 600px) {
+  .puzzle-comp-cont {
+    flex-direction: column;
+    height: auto;
+  }
+  .navImgs {
+    padding: 10px;
+    flex-direction: row;
+    width: 100%;
+    max-width: none;
+    height: 20vh;
+  }
+  .game {
+    width: 90vw;
+    height: 90vw;
+  }
+  .options {
+    width: 90vw;
+  }
+}
+/* game style*/
+#backCnv {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.3;
+  background-color: var(--colorD2);
+}
+.puzzle {
+  display: grid;
+  grid-template-areas: "a0 a1 a2" "a3 a4 a5" "a6 a7 a8";
+  gap: 1px 1px;
+  width: 100%;
+  height: 100%;
+}
+.section {
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+}
+.section * {
+  position: absolute;
+}
+.section span {
+  z-index: 1;
+  font-size: 2em;
+  user-select: none;
+}
+.section canvas {
+  width: 100%;
+  height: 100%;
+}
+#section-8 {
+  visibility: hidden;
+}
+.message:deep() span {
+  color: rgb(30, 255, 40);
+}
+.path {
+  margin-bottom: 10px;
+  padding: 5px;
+  overflow-x: auto;
+  white-space: nowrap;
+  background-color: var(--colorD2);
+}
+</style>
